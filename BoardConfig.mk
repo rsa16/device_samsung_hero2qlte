c695c01da582b9ade73fce154534393bdf0fb646
Rehan <47797856+rsa16@users.noreply.github.com>
2023-01-06 22:44:18 -0500

    add hero2qlte specific boardconfigs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,6 +18,59 @@ include device/samsung/universal8890-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/samsung/hero2qlte
 
+# Bootloader
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := msm8996
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8996
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno530
+TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := kryo
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+# Kernel
+#TARGET_KERNEL_SOURCE := kernel/samsung/msm8996
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_DEVICE_DEFCONFIG := device_hero2qlte_tmo
+
+TARGET_PREBUILT_KERNEL := $(DEVICE_TREE)/Image.gz
+TARGET_PREBUILT_DTB := $(DEVICE_TREE)/dtb.img
+
+# Boot image
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.bootdevice=624000.ufshc androidboot.selinux=permissive user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=24M@0-0xffffffff rcupdate.rcu_expedited=1
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_PAGESIZE := 4096
+# 000RU = recovery kernel, 000KU = system kernel
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02200000 --tags_offset 0x02000000 --board SRPPA14B001RU
+BOARD_CUSTOM_BOOTIMG_MK :=  $(DEVICE_TREE)/bootimg.mk
+
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE     := 0x004C00000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x004C00000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE   := 0x14E600000
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x64BFFC000 # 0x64C000000 - 16384 (footer)
+BOARD_CACHEIMAGE_PARTITION_SIZE    := 0x012C00000
+BOARD_FLASH_BLOCK_SIZE := 0x40000
+
+# File systems
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := hero2ltebmc,hero2lteskt,hero2ltektt,hero2ltelgt,hero2ltexx,hero2qlte,hero2qltexx
 
@@ -32,7 +85,6 @@ TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_hero2lte
 
 # Kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/universal8890
-TARGET_KERNEL_CONFIG := exynos8890-hero2lte_defconfig
 
 # Shims
 TARGET_LD_SHIM_LIBS += \
@@ -44,6 +96,12 @@ PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
 
 # Vendor security patch level
 VENDOR_SECURITY_PATCH := 2021-04-01
+
+# Init properties from bootloader version, ex. model info
+TARGET_UNIFIED_DEVICE := true
+TARGET_INIT_VENDOR_LIB := libinit_hero2qlte
+TARGET_RECOVERY_DEVICE_MODULES := libinit_hero2qlte
+TARGET_LIBINIT_DEFINES_FILE := $(DEVICE_TREE)/init/init_hero2qlte.cpp
 
 # inherit from the proprietary version
 include vendor/samsung/hero2qlte/BoardConfigVendor.mk
